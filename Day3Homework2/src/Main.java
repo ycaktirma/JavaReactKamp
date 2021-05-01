43bc677146fea5d3e4416637a4ec9fb44a02b8ce
ycaktirma <ycaktirma@gmail.com>
2021-05-01 21:12:17 +0300

    Main Revise

--- Day3Homework2/src/Main.java
+++ Day3Homework2/src/Main.java
@@ -3,25 +3,25 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Homework dayTwoHomeworkOne = new Homework();
-		dayTwoHomeworkOne.set_id(0);
-		dayTwoHomeworkOne.set_title("Ödev 1");
-		dayTwoHomeworkOne.set_description("Aþaðýdaki oynatma listesini 27-39 aralýðýnda izleyiniz.(27-39 Dahil) Kendiniz de uygulayýnýz.");
-		dayTwoHomeworkOne.set_situation(false);
+		Homework dayThreeHomeworkOne = new Homework();
+		dayThreeHomeworkOne.set_id(0);
+		dayThreeHomeworkOne.set_title("Ödev 1");
+		dayThreeHomeworkOne.set_description("Aþaðýdaki oynatma listesini 27-39 aralýðýnda izleyiniz.(27-39 Dahil) Kendiniz de uygulayýnýz.");
+		dayThreeHomeworkOne.set_situation(false);
 		
-		Homework dayTwoHomeworkTwo = new Homework();
-		dayTwoHomeworkTwo.set_id(0);
-		dayTwoHomeworkTwo.set_title("Ödev 2");
-		dayTwoHomeworkTwo.set_description("User, Instructor, Student nesnelerini modelleyiniz. (Class-Inheritance)");
-		dayTwoHomeworkTwo.set_situation(false);
+		Homework dayThreeHomeworkTwo = new Homework();
+		dayThreeHomeworkTwo.set_id(0);
+		dayThreeHomeworkTwo.set_title("Ödev 2");
+		dayThreeHomeworkTwo.set_description("User, Instructor, Student nesnelerini modelleyiniz. (Class-Inheritance)");
+		dayThreeHomeworkTwo.set_situation(false);
 		
 		Homework[] dayThreeHomeworksEmirhan = new Homework[]
 		{
-			dayTwoHomeworkOne
+				dayThreeHomeworkOne
 		};
 		Homework[] dayThreeHomeworksYarkin = new Homework[]
 		{
-			dayTwoHomeworkTwo
+				dayThreeHomeworkTwo
 		};
 		
 		Student yarkin = new Student();
